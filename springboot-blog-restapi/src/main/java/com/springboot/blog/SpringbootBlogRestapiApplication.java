e6adb8508b201d7245c03f71339b8c086c3590be
Max2 <chigov@gmail.com>
2022-05-03 22:17:01 +0300

    Initial commit

---
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/SpringbootBlogRestapiApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestapiApplication.class, args);
+		System.out.println("test");
+
+	}
+
+}
