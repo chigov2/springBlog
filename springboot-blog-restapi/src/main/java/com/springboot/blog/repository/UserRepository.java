b86094d2b5cc4ad02934c41c726bf15d5941c437
chigov2 <chigov@gmail.com>
2022-05-15 19:26:45 +0300

    75

--- springboot-blog-restapi/src/main/java/com/springboot/blog/repository/UserRepository.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.springboot.blog.repository;
 
-public interface UserRepository {
+import com.springboot.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByEmailOrUserName(String email,String userName);
+
+    //exist user in database or not
+    Boolean existByUserName(String userName);
+    Boolean existByEmail(String email);
+
 }
