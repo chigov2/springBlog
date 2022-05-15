4368618eec4fd0090b6b4e0adc10aa1309874978
Max2 <chigov@gmail.com>
2022-05-15 11:17:43 +0300

    73

--- springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.ApiConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,6 +21,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     //create post REST API
     @PostMapping//important!!!!!!!!!!!!!!!!!!
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
@@ -43,6 +45,7 @@ public class PostController {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     //update post by id rest api
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name = "id") long id){
@@ -52,6 +55,7 @@ public class PostController {
         return new ResponseEntity(postResponse, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     //delete post by id
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePopst(@PathVariable(name = "id") long id){
