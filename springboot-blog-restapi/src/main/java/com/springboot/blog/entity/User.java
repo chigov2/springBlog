aa6be2f60c5181b120bb11a01fc5fc657c938a99
chigov2 <chigov@gmail.com>
2022-05-15 16:43:15 +0300

    74

---
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/entity/User.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users",uniqueConstraints ={
+@UniqueConstraint(columnNames = "userName"),
+@UniqueConstraint(columnNames = "email")})
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //primary key - become a forieng key for user_roles table
+    private long id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role> roles;
+}
