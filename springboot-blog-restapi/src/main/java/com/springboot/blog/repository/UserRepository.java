648ef661214685d293d6366c35ed44ae35f54042
Max2 <chigov@gmail.com>
2022-05-16 08:24:41 +0300

    75 with error in UserRepository

--- springboot-blog-restapi/src/main/java/com/springboot/blog/repository/UserRepository.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByUserNameOrEmail(String userName, String email);
     //exist user in database or not
     Boolean existByUserName(String userName);
     Boolean existByEmail(String email);
