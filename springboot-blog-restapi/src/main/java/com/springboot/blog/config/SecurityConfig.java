8d1780875ca616a0152a8bd9ff8dfa26e98a682f
Max2 <chigov@gmail.com>
2022-05-17 18:48:01 +0300

    85

--- springboot-blog-restapi/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,7 +48,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
     }
 
-//    @Override
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //    @Override
 //    @Bean
 //    protected UserDetailsService userDetailsService() {
 //        UserDetails mikee = User.builder().username("mikee").password(passwordEncoder().encode("158500")).roles("USER").build();
---
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public ResponseEntity<String> authenticateUser()
+
+}
