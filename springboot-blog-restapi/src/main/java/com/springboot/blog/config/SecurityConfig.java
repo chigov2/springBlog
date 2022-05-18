22622c0a91dd8589061eee00261c3f23c2aa089a
Max2 <chigov@gmail.com>
2022-05-18 09:44:06 +0300

    86_fixed

--- springboot-blog-restapi/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.config;
 
+
 import com.springboot.blog.security.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- springboot-blog-restapi/src/main/java/com/springboot/blog/controller/AuthController.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/controller/AuthController.java
@@ -40,38 +40,38 @@ public class AuthController {
     @PostMapping("/signin")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginDto.getUsernameOrEmail(),
-                loginDto.getPassword()));
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed in succesfully", HttpStatus.OK);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto){
 
-        //check if username exist
-        if (userRepository.existsByUserName(signUpDto.getUsername())){
-            return new ResponseEntity<>("User already exist.",HttpStatus.BAD_REQUEST);
-        }
-        //check if email already exist
-        if (userRepository.existsByEmail(signUpDto.getEmail())){
-            return  new ResponseEntity<>("Email already taken.",HttpStatus.BAD_REQUEST);
+        // add check for username exists in a DB
+        if(userRepository.existsByUsername(signUpDto.getUsername())){
+            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
         }
 
-        //create user object
+        // add check for email exists in DB
+        if(userRepository.existsByEmail(signUpDto.getEmail())){
+            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        // create user object
         User user = new User();
         user.setName(signUpDto.getName());
         user.setUsername(signUpDto.getUsername());
         user.setEmail(signUpDto.getEmail());
-        //must be encoded
         user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
 
         Role roles = roleRepository.findByName("ROLE_ADMIN").get();
-        user.setRoles(Collections.singleton(roles));////////////////////////////////////////
+        user.setRoles(Collections.singleton(roles));
 
-        //save info to DB
         userRepository.save(user);
-            return new ResponseEntity<>("user added successfully.",HttpStatus.OK);
-    }
 
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+
+    }
 }
