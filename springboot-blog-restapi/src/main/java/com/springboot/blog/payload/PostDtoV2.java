a159ff39b051968cef0e6dc0b8971aeead2bdf0f
Max2 <chigov@gmail.com>
2022-05-19 12:12:47 +0300

    97

---
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/payload/PostDtoV2.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class PostDtoV2 {
+    private long id;
+    //title should not be null or empty
+    //title should have at least 2 characters
+    @NotEmpty(message = "Must not be empty")
+    @Size(min = 2, message = "Title must have more then 2 characters")
+    private String title;
+
+    //must be not null or empty
+    //should have at least 10 characters
+    @NotEmpty(message = "Must not be empty")
+    @Size(min = 10, message = "Description must have at least 10 characters")
+    private String description;
+
+    @NotEmpty(message = "Content must not be empty")
+    private String content;
+    private Set<CommentDto> comments;
+    private List<String> tags;
+}
