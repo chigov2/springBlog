9b0eb8110e1e1b5b08abadb27ddc10b53b2105a7
Max2 <chigov@gmail.com>
2022-05-21 15:23:39 +0300

    102

--- springboot-blog-restapi/src/main/java/com/springboot/blog/config/SwaggerConfig.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,6 +37,8 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -43,11 +46,14 @@ public class SwaggerConfig {
     }
 
     private SecurityContext securityContext(){
-        return SecurityContext.builder().securityReferences().build();
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
     }
 
     private List<SecurityReference> defaultAuth(){
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT",authorizationScopes));
     }
 
 }
