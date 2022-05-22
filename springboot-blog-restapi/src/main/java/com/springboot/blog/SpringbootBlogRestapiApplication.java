321607621479d6251f482e3a6531318fead2a3e8
chigov2 <chigov@gmail.com>
2022-05-22 20:16:59 +0300

    110_22_05_2022

--- springboot-blog-restapi/src/main/java/com/springboot/blog/SpringbootBlogRestapiApplication.java
+++ springboot-blog-restapi/src/main/java/com/springboot/blog/SpringbootBlogRestapiApplication.java
@@ -1,12 +1,19 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootBlogRestapiApplication {
+public class SpringbootBlogRestapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private RoleRepository roleRepository;
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -17,4 +24,15 @@ public class SpringbootBlogRestapiApplication {
 		SpringApplication.run(SpringbootBlogRestapiApplication.class, args);
 		}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
